81c88440a0e56d72d237dba916f6ce8d9581aa60
Reason-With-Talent <oluwatobibalogun2@gmai.com>
2023-10-18 14:43:37 -0400

    update files

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -3,6 +3,7 @@ import Basic from "../assets/img/Basic.svg"
 import Professional from "../assets/img/Professional.svg"
 import Enterprise from "../assets/img/Enterprise.svg"
 import Question from "../assets/img/Questions.svg"
+import { AiOutlineRight } from "react-icons/ai"
 const Pricing = () => {
     return (
         <>
@@ -39,27 +40,33 @@ const Pricing = () => {
                     <div className="md:w-[20%] m-auto pt-[50px]">
                         <h1 className="text-[#2D1D60] font-semibold text-[25px] text-center pb-[15px] ">FREQUENTLY ASKED QUESTIONS</h1>
                     </div>
-                    <div className="grid justify-center pt-[40px] md:flex md:justify-between">
-                        <div>
-                            <div className="pb-[20px]">
-                                <img src={Question} />
+                    <div className="grid justify-center pt-[40px] md:flex">
+                        <div className="md:pr-[30px]">
+                            <div className="shadow-2xl md:bg-[#FFFFFF] bg-[red;] shadow-none flex md:w-[500px] w-[410px] h-[120px] md:h-[150px] mb-[30px]">
+                                <p className="text-[#2D1D60;] text-[18px] font-medium m-auto text-center md:p-[0px] p-[10px] md:w-[58%]"> Can I automate multiple certificates at once to different users?</p>
+                                <AiOutlineRight className="text-[40px] md:mt-[50px] mt-[40px] md:pr-[0px]" />
                             </div>
-                            <div className="pb-[20px]">
-                                <img src={Question} />
+                            <div className="shadow-2xl md:bg-[#FFFFFF] bg-[red;] shadow-none flex md:w-[500px] md:h-[150px] w-[410px] h-[120px] mb-[30px]">
+                                <p className="text-[#2D1D60;] text-[18px] font-medium m-auto text-center md:w-[58%]"> Which platforms are supported?</p>
+                                <AiOutlineRight className="text-[40px] md:mt-[50px] mt-[40px] md:pr-[0px]" />
                             </div>
-                            <div className="pb-[20px]">
-                                <img src={Question} />
+                            <div className="shadow-2xl md:bg-[#FFFFFF] bg-[red;] shadow-none flex md:w-[500px] md:h-[150px] h-[120px] w-[410px] mb-[30px]">
+                                <p className="text-[#2D1D60;] text-[18px] font-medium m-auto text-center md:w-[58%]"> Does Certifying support external Sheet integrations?</p>
+                                <AiOutlineRight className="text-[40px] md:mt-[50px] mt-[40px] md:pr-[0px]" />
                             </div>
                         </div>
-                        <div>
-                            <div className="pb-[20px]">
-                                <img src={Question} />
+                        <div className="md:pl-[30px]">
+                            <div className="shadow-2xl md:bg-[#FFFFFF] bg-[red;] shadow-none flex md:w-[500px] md:h-[150px] h-[120px] w-[410px] mb-[30px]">
+                                <p className="text-[#2D1D60;] text-[18px] font-medium m-auto text-center md:w-[58%]"> Is the Application Free?</p>
+                                <AiOutlineRight className="text-[40px] md:mt-[50px] mt-[40px] md:pr-[0px]" />
                             </div>
-                            <div className="pb-[20px]">
-                                <img src={Question} />
+                            <div className="shadow-2xl md:bg-[#FFFFFF] bg-[red;] shadow-none flex md:w-[500px] md:h-[150px] h-[120px] w-[410px] mb-[30px]">
+                                <p className="text-[#2D1D60;] text-[18px] font-medium m-auto text-center md:w-[58%]">Can I customize the Template?</p>
+                                <AiOutlineRight className="text-[40px] md:mt-[50px] mt-[40px] md:pr-[0px]" />
                             </div>
-                            <div className="pb-[20px]">
-                                <img src={Question} />
+                            <div className="shadow-2xl md:bg-[#FFFFFF] bg-[red;] shadow-none flex md:w-[500px] md:h-[150px] h-[120px] w-[410px] mb-[30px]">
+                                <p className="text-[#2D1D60;] text-[18px] font-medium m-auto text-center md:w-[58%]"> Can I use an existing Design?</p>
+                                <AiOutlineRight className="text-[40px] md:mt-[50px] mt-[40px] md:pr-[0px]" />
                             </div>
                         </div>
                     </div>
